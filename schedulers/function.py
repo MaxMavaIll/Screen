d968055e16e5bc04568b7a1b064380406ec9c6e9
MaxMavaIll <illvauser8@gmail.com>
2023-06-03 22:54:35 +0300

    schedulers was converted

---
+++ schedulers/function.py
@@ -0,0 +1,118 @@
+import redis, logging, toml, time
+from datetime import datetime, timedelta
+
+from aiogram import Bot
+
+from tgbot.config import Config
+
+
+def get_already_db() -> list[int]:
+    r = redis.Redis(host='localhost', port=6379, password='Hallo')
+
+    # Отримання інформації про Redis сервер
+    info = r.info()
+
+    # Отримання списку баз даних
+    databases = []
+    for key in info.keys():
+        if key.startswith('db'):
+            db_number = key[2:].split(':')[0]
+            databases.append(int(db_number))
+
+    return databases
+    
+def get_db():
+    config_toml = toml.load("config.toml")
+    databases_already = get_already_db()
+    for key in config_toml.keys():
+        networks = list(config_toml[key].keys())
+        number_key = len(networks)
+        
+        i = j = 1
+        while(j < number_key + 1):
+            if config_toml[key][networks[j-1]]["db"] != 0:
+                i = config_toml[key][networks[j-1]]["db"] + 1
+                j+=1
+                continue
+
+            if i in databases_already:
+                i+=1
+                continue
+            
+            config_toml[key][networks[j-1]]["db"] = i
+            i+=1
+            j += 1
+            
+
+        with open('config.toml', 'w') as file:
+            toml.dump(config_toml, file)
+    
+    
+
+
+def get_keys_redis(config: Config, db: int ) -> list:
+    
+    keys = []
+    
+    r = redis.Redis(
+        host=config.redis_config.host, 
+        port=config.redis_config.port, 
+        db=db,
+        password=config.redis_config.password
+        )
+
+    keys_list = r.keys('*')
+
+    logging.debug(keys_list)
+
+    for key in keys_list:
+        if "data" in key.decode("utf-8"):
+            keys.append(key.decode("utf-8"))
+
+    logging.info(f"Number of clients: {len(keys)}\n")
+
+
+    return keys
+
+def check_number_missed_blocks(first_result: int, second_result: int, allow_missed_block: int):
+    rizn = second_result - first_result
+
+    logging.info(f"The number of missed blocks for 10 min:  {rizn}\n")
+
+    if rizn >= allow_missed_block:
+        logging.info(f"More than allowed {allow_missed_block}\n")
+        return True
+    
+    return False
+
+
+async def send_message_user(bot: Bot, 
+                            chat_id: int, 
+                            moniker: str, 
+                            missed_blocks: int, 
+                            
+                            ):
+    config = toml.load("config.toml")
+
+    skipped_blocks_allowed = config["signed_blocks_window"] * (1 - config["min_signed_per_window"] / 100)
+
+
+
+    time_to_jail_seconds = (skipped_blocks_allowed - missed_blocks) * config["time_create_block"]
+    # time_to_jail = datetime.timedelta(seconds=)
+    # time_to_jail = datetime.now() + timedelta(seconds=time_to_jail_seconds)
+    time_to_jail = time.strftime("%H:%M", time.gmtime(time_to_jail_seconds))
+
+
+    if missed_blocks > (skipped_blocks_allowed * 0.7):
+        await bot.send_message(chat_id, f"<b>Moniker: {moniker}.</b>"
+                            f"\n<b>I've just found {missed_blocks} missed blocks out of {skipped_blocks_allowed} total.</b>"
+                            f"\n<b>You have ~{time_to_jail} hours before jailing.</b>"
+                            f"\n <b>If you don't fix it, your validator will go to jail.</b>")
+
+    else:
+        await bot.send_message(chat_id, f"<b>Moniker: {moniker}.</b>"
+                            f"\nI've just found {missed_blocks} missed blocks out of {skipped_blocks_allowed} total."
+                            f"\nYou have ~{ time_to_jail } hours before jailing.")
+        
+    logging.info(f"I sent message to {chat_id}")
---
+++ schedulers/job_new.py
@@ -0,0 +1,92 @@
+import asyncio, toml, json, logging
+import math
+
+from aiogram import Bot
+from aiogram.dispatcher.fsm.storage.redis import RedisStorage, StorageKey
+
+from tgbot.config import Config
+
+# from aiogram.dispatcher.fsm.context import FSMContext
+
+from schedulers.function import get_keys_redis,check_number_missed_blocks, send_message_user, get_db
+from schedulers.exceptions import NoSlashingInfo, raise_error
+from schedulers.exceptions import raise_error
+from funtion import * #, get_index_by_address
+
+
+
+async def check_user_node( 
+        bot: Bot,
+        config: Config,
+        storage: RedisStorage,
+        type_network: str,
+        network: str
+        ) -> None:
+    
+    get_db()
+
+    config_toml = toml.load("config.toml")
+    
+
+   
+    time_repeat = config_toml[type_network][network]["time_repeat"]
+    allow_missed_block = config_toml[type_network][network]["missed_blocks"]
+    urls = await check_url(config_toml[type_network][network]["rpc"])
+
+    logging.info(f"type_network: {type_network} | network: {network}")
+    if urls["active_urls"] == []:
+
+        for id in config.tg_bot.admin_ids:
+            logging.info(id)
+            await bot.send_message(
+            chat_id=id,
+            text=f'RPC not working 🔴, network {network} SCREENVALIDATOR',
+            )
+
+        return 
+
+    url = urls["active_urls"][0]
+
+    keys = get_keys_redis(config, config_toml[type_network][network]["db"])
+
+
+    for key in keys:
+        key = key.split(":")
+
+        bot_id = int(key[1])
+        chat_id = int(key[2])
+        user_id = int(key[3])
+        
+        logging.info(f"User: id - {user_id}")
+
+        data = await storage.get_data(bot, StorageKey(bot_id=bot_id, chat_id=chat_id, user_id=user_id))
+        list_users = list(data["validators"].keys())
+        logging.info(f"Users: {len(list_users)}")
+
+
+        for moniker in data["validators"].keys():
+            logging.info(f"Moniker: {moniker}")
+
+            # validators = await get_validators(url)
+            # validator = validators[await get_index_by_moniker(moniker, validators)]
+            # signing_info = await slashing_signing_info(validator.get("consensus_pubkey").get("key"), url)
+            signing_infos = await slashing_signing_info_all(url)
+            signing_info = signing_infos[await get_index_by_consAddr(data["validators"][moniker]["const_addr"], signing_infos)]
+
+            first_snapshot_missed_block = data["validators"][moniker]["last_missed_block"]
+            second_snapshot_missed_block = int(signing_info.get("missed_blocks_counter"))
+
+            if check_number_missed_blocks(first_snapshot_missed_block, second_snapshot_missed_block, allow_missed_block):
+
+                await send_message_user(bot=bot, chat_id=chat_id, moniker=moniker, missed_blocks=second_snapshot_missed_block)
+
+            data["validators"][moniker]["last_missed_block"] = second_snapshot_missed_block
+
+        
+
+        data["rpc"] = urls
+
+
+        await storage.update_data(bot, StorageKey(bot_id=bot_id, chat_id=chat_id, user_id=user_id), data)
+        logging.debug(f"Data sheduler: {data}")
+
